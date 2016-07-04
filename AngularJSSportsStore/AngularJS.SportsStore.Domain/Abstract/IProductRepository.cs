bd9aca5f84a1be590bf60573c03f59013bb534c5
HarryPaek <paek.harry@gmail.com>
2016-07-04 15:27:49 +0900

    Project Init

---
+++ AngularJSSportsStore/AngularJS.SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using AngularJS.SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularJS.SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productID);
+    }
+}
